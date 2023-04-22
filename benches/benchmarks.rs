26af243e5d1d0516997011efb03d5bceb35e1535
0xKitsune <0xKitsune@protonmail.com>
2023-04-21 21:36:25 -0400

    updated to search in batches with par_iter

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -17,10 +17,34 @@ pub fn count_zero_bytes_benchmark(c: &mut Criterion) {
     });
 }
 
+pub fn find_optimal_batch_size_benchmark(c: &mut Criterion) {
+    const SEED: &str = "0101010101";
+    c.bench_function("Find optimal batch size", |b| {
+        b.iter(|| zero_seeker::find_optimal_batch_size(SEED, false))
+    });
+}
+
+pub fn mine_address_with_n_zero_bytes_benchmark(c: &mut Criterion) {
+    const SEED: &str = "0101010101";
+
+    c.bench_function("Mine address with 2 zero bytes", |b| {
+        b.iter(|| zero_seeker::mine_address_with_n_zero_bytes(SEED, 2, false, 200))
+    });
+}
+
+// pub fn mine_address_with_n_leading_zero_bytes_benchmark(c: &mut Criterion) {
+//     const SEED: &str = "0101010101";
+//     c.bench_function("Mine address with 2 zero bytes", |b| {
+//         b.iter(|| zero_seeker::mine_address_with_n_zero_bytes(SEED, 2, true))
+//     });
+// }
+
 criterion_group!(
     benches,
-    count_leading_zero_bytes_benchmark,
-    count_zero_bytes_benchmark
+    // count_leading_zero_bytes_benchmark,
+    // count_zero_bytes_benchmark,
+    // find_optimal_batch_size_benchmark,
+    mine_address_with_n_zero_bytes_benchmark
 );
 
 criterion_main!(benches);
